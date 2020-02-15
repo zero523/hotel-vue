1c51b0df5c906ddc710c8c546a61924ee15b5b27
zero523 <425727235@qq.com>
2020-02-15 20:33:22 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+		hotel
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+	@import '~assets/css/normalize.css';
+</style>
